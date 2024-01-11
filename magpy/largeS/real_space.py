7dc5a1282d38f9752669ab57b326f95e1c0db541
Jonas Habel <jonas.habel@tum.de>
2024-01-11 15:01:04 +0100

    Bugfixes in real-space magnon Hamiltonian for arbitrary order

--- magpy/largeS/real_space.py
+++ magpy/largeS/real_space.py
@@ -36,10 +36,10 @@ def compute_magnon_Hamiltonian(model: Model, order: int):
                 magnon_BdG_tensor[CREATOR, ANNIHILATOR] = -spin_int_tensor[2]
             elif order % 2 == 1:
                 t = (order-1) // 2
-                idx    = (*((CREATOR, ANNIHILATOR)*t), ANNIHILATOR)
-                idx_hc = __hc(idx)
-                magnon_BdG_tensor[idx]    = C[t] * spin_int_tensor[0]
-                magnon_BdG_tensor[idx_hc] = C[t] * spin_int_tensor[1]
+                idx1    = (*((CREATOR, ANNIHILATOR)*t), ANNIHILATOR)
+                idx1_hc = __hc(idx1)
+                magnon_BdG_tensor[idx1]    = C[t] * spin_int_tensor[0]
+                magnon_BdG_tensor[idx1_hc] = C[t] * spin_int_tensor[1]
             else:
                 pass # no even-order terms if order >= 4
             magnon_H_i = Interaction([site]*2, magnon_BdG_tensor)
@@ -101,26 +101,30 @@ def compute_magnon_Hamiltonian(model: Model, order: int):
                 magnon_BdG_tensor_ii_js = np.zeros((2,)*order, dtype=np.complex128)
                 magnon_BdG_tensor_is_jj = np.zeros((2,)*order, dtype=np.complex128)
                 magnon_BdG_tensor_js_ii = np.zeros((2,)*order, dtype=np.complex128)
-                idx = (
-                    *((CREATOR, ANNIHILATOR)*t), ANNIHILATOR, # site i or site j
+                idx1 = (
+                    *((CREATOR, ANNIHILATOR)*t), ANNIHILATOR,
                 )
-                idx_hc = __hc(idx)
+                idx2 = (
+                    *((CREATOR, ANNIHILATOR)*(t-1)), ANNIHILATOR, CREATOR, ANNIHILATOR,
+                )
+                idx1_hc = __hc(idx1)
+                idx2_hc = __hc(idx2)
 
-                magnon_BdG_tensor_all_i[idx] = C[t] * S_i**(1/2-t) * S_j * spin_int_tensor[0, 2]
-                magnon_BdG_tensor_all_j[idx] = C[t] * S_i * S_j**(1/2-t) * spin_int_tensor[2, 0]
-                magnon_BdG_tensor_jj_is[idx] = -C[t-1] * S_i**(3/2-t) * spin_int_tensor[0, 2]
-                magnon_BdG_tensor_ii_js[idx] = -C[t-1] * S_j**(3/2-t) * spin_int_tensor[2, 0]
-                magnon_BdG_tensor_all_i[idx_hc] = C[t] * S_i**(1/2-t) * S_j * spin_int_tensor[1, 2]
-                magnon_BdG_tensor_all_j[idx_hc] = C[t] * S_i * S_j**(1/2-t) * spin_int_tensor[2, 1]
-                magnon_BdG_tensor_is_jj[idx_hc] = -C[t-1] * S_i**(3/2-t) * spin_int_tensor[1, 2]
-                magnon_BdG_tensor_js_ii[idx_hc] = -C[t-1] * S_j**(3/2-t) * spin_int_tensor[2, 1]
+                magnon_BdG_tensor_all_i[idx1] = C[t] * S_i**(1/2-t) * S_j * spin_int_tensor[0, 2]
+                magnon_BdG_tensor_all_j[idx1] = C[t] * S_i * S_j**(1/2-t) * spin_int_tensor[2, 0]
+                magnon_BdG_tensor_jj_is[idx2] = -C[t-1] * S_i**(3/2-t) * spin_int_tensor[0, 2]
+                magnon_BdG_tensor_ii_js[idx1] = -C[t-1] * S_j**(3/2-t) * spin_int_tensor[2, 0]
+                magnon_BdG_tensor_all_i[idx1_hc] = C[t] * S_i**(1/2-t) * S_j * spin_int_tensor[1, 2]
+                magnon_BdG_tensor_all_j[idx1_hc] = C[t] * S_i * S_j**(1/2-t) * spin_int_tensor[2, 1]
+                magnon_BdG_tensor_is_jj[idx2_hc] = -C[t-1] * S_i**(3/2-t) * spin_int_tensor[1, 2]
+                magnon_BdG_tensor_js_ii[idx1_hc] = -C[t-1] * S_j**(3/2-t) * spin_int_tensor[2, 1]
 
                 Hamiltonians += [
                     Interaction([site_i]*order, magnon_BdG_tensor_all_i),
                     Interaction([site_j]*order, magnon_BdG_tensor_all_j),
-                    Interaction([site_j]*2 + [site_i]*(order-2), magnon_BdG_tensor_jj_is),
+                    Interaction([site_i]*(order-2) + [site_j]*2, magnon_BdG_tensor_jj_is),
                     Interaction([site_i]*2 + [site_j]*(order-2), magnon_BdG_tensor_ii_js),
-                    Interaction([site_i]*(order-2) + [site_j]*2, magnon_BdG_tensor_is_jj),
+                    Interaction([site_j]*2 + [site_i]*(order-2), magnon_BdG_tensor_is_jj),
                     Interaction([site_j]*(order-2) + [site_i]*2, magnon_BdG_tensor_js_ii),
                 ]
                 
@@ -155,109 +159,3 @@ def compute_magnon_Hamiltonian(model: Model, order: int):
         Hamiltonians))
 
     return Hamiltonians
-
-
-"""
-model: Model
-order: integer
-    order of the vertex factors in 1/sqrt(S)
-    e.g. order=2 -> LSWT, O(S); order=3 -> O(sqrt(S)); ...
-
-We use the Holstein-Primakoff mapping. Each spin S' is mapped as
-    S_i^z = S - a_i^dagger a_i
-    S_i^+ = sqrt(2S - a_i^dagger a_i) a_i
-    S_i^- = a_i^dagger sqrt(2S - a_i^dagger)
-The square root terms are then expanded in powers of 1/sqrt(S):
-    sqrt(2S - a_i^dagger a_i) = sqrt(2S) - a_i^dagger a_i/sqrt(8S) + ...
-and terms of order <code>order</code> are then collected.
-
-returns: list of Interaction
-    each interaction is specified by a BdG-type interaction_tensor
-    e.g. order=2 -> [a_i, a_i^dagger] H_bdg [a_j a_j^dagger]
-"""
-def compute_LSWT_Hamiltonian_real_space(model: Model):
-    if any(map(lambda inter: len(inter.sites) not in [1, 2], model.interactions)):
-        raise NotImplementedError("so far, only implemented for one- or two-spin interactions.")
-    
-    C = LARGE_S_EXPANSION_COEFF # rename for brevity
-    S = model.get_onsite_spin_quantum_numbers()
-    # ca = creator annihilator = a_i^† a_i
-    ca = np.array([[0, 0], [1, 0]])
-
-    rotated_spin_interactions = model.compute_rotated_interactions()
-    LSWT_Hamiltonians = []
-    for inter in rotated_spin_interactions:
-        spin_int_tensor = inter.interaction_tensor
-        if len(inter.sites) == 1:
-            site = inter.sites[0]
-            magnon_BdG_tensor_ii = -spin_int_tensor[2] * ca
-            magnon_H_ii = Interaction([site]*2, magnon_BdG_tensor_ii)
-            LSWT_Hamiltonians += [magnon_H_ii]
-        elif len(inter.sites) == 2:
-            site_i, site_j = inter.sites
-            S_ij = np.sqrt(S[site_i.subl_idx] * S[site_j.subl_idx])
-            S_ii = S[site_j.subl_idx]
-            S_jj = S[site_i.subl_idx]
-            magnon_BdG_tensor_ij =  S_ij * C[0]**2 * spin_int_tensor[0:2, 0:2]
-            magnon_BdG_tensor_ii = -S_ii * spin_int_tensor[2, 2] * ca
-            magnon_BdG_tensor_jj = -S_jj * spin_int_tensor[2, 2] * ca
-            magnon_H_ij = Interaction(inter.sites, magnon_BdG_tensor_ij)
-            magnon_H_ii = Interaction([site_i]*2, magnon_BdG_tensor_ii)
-            magnon_H_jj = Interaction([site_j]*2, magnon_BdG_tensor_jj)
-            LSWT_Hamiltonians += [
-                magnon_H_ij, magnon_H_ii, magnon_H_jj,
-            ]   
-    
-    return LSWT_Hamiltonians
-
-
-
-def compute_interaction_Hamiltonian(model: Model, order):
-    if order not in [3] or any(map(lambda inter: len(inter.sites) not in [1, 2], model.interactions)):
-        raise NotImplementedError("so far, only implemented for cubic vertices of one- or two-spin interactions.")
-    
-    C = LARGE_S_EXPANSION_COEFF # rename for brevity
-    S = model.get_onsite_spin_quantum_numbers()
-    # caa = creator annihilator annihilator = a^† a a
-    caa = np.zeros((2, 2, 2))
-    caa[1, 0, 0] = 1
-    # cca = creator creator annihilator = a^† a^† a
-    cca = np.zeros((2, 2, 2))
-    cca[1, 1, 0] = 1
-
-    rotated_spin_interactions = model.compute_rotated_interactions()
-    magnon_Hamiltonians = []
-    for inter in rotated_spin_interactions:
-        spin_int_tensor = inter.interaction_tensor
-        if len(inter.sites) == 1:
-            site = inter.sites[0]
-            magnon_BdG_tensor_iii = C[1] * (spin_int_tensor[0] * caa \
-                                          + spin_int_tensor[1] * cca)
-            magnon_H_iii = Interaction([site]*3, magnon_BdG_tensor_iii)
-            magnon_Hamiltonians += [magnon_H_iii]
-        elif len(inter.sites) == 2:
-            site_i, site_j = inter.sites
-            S_iii = S[site_j.subl_idx] / np.sqrt(S[site_i.subl_idx])
-            S_jjj = S[site_i.subl_idx] / np.sqrt(S[site_j.subl_idx])
-            S_jij = S_ijj = np.sqrt(S[site_i.subl_idx])
-            S_iij = S_iji = np.sqrt(S[site_j.subl_idx])
-            magnon_BdG_tensor_iii = S_iii * C[1] * (spin_int_tensor[0, 2] * caa\
-                                                  + spin_int_tensor[1, 2] * cca)
-            magnon_BdG_tensor_jjj = S_jjj * C[1] * (spin_int_tensor[2, 0] * caa\
-                                                  + spin_int_tensor[2, 1] * cca)
-            magnon_BdG_tensor_jij = -S_jij * C[0] * spin_int_tensor[0, 2] * caa
-            magnon_BdG_tensor_ijj = -S_ijj * C[0] * spin_int_tensor[1, 2] * cca
-            magnon_BdG_tensor_iij = -S_iij * C[0] * spin_int_tensor[2, 0] * caa
-            magnon_BdG_tensor_iji = -S_iji * C[0] * spin_int_tensor[2, 1] * cca
-            magnon_H_iii = Interaction([inter.sites[0]]*3, magnon_BdG_tensor_iii)
-            magnon_H_jjj = Interaction([inter.sites[1]]*3, magnon_BdG_tensor_jjj)
-            magnon_H_jij = Interaction([inter.sites[n] for n in [1, 0, 1]], magnon_BdG_tensor_jij)
-            magnon_H_ijj = Interaction([inter.sites[n] for n in [0, 1, 1]], magnon_BdG_tensor_ijj)
-            magnon_H_iij = Interaction([inter.sites[n] for n in [0, 0, 1]], magnon_BdG_tensor_iij)
-            magnon_H_iji = Interaction([inter.sites[n] for n in [0, 1, 0]], magnon_BdG_tensor_iji)
-            magnon_Hamiltonians += [
-                magnon_H_iii, magnon_H_jjj, magnon_H_jij,
-                magnon_H_ijj, magnon_H_iij, magnon_H_iji,
-            ]
-    
-    return magnon_Hamiltonians
--- tests/largeS_test.py
+++ tests/largeS_test.py
@@ -169,6 +169,7 @@ def test_real_space_Hamiltonian_honeycomb_DMI():
 
     # order S^2
     expected_magnon_H_0 = [
+        # Heisenberg
         Interaction([], np.array(J*S_A*S_B)),
         Interaction([], np.array(J*S_A*S_B)),
         Interaction([], np.array(J*S_A*S_B)),
@@ -177,12 +178,14 @@ def test_real_space_Hamiltonian_honeycomb_DMI():
     # order S^(3/2)
     prefactor_1 = D*np.sin(theta)*1j/np.sqrt(2)
     expected_magnon_H_1 = [
+        # DMI-x
         Interaction([site_iA], prefactor_1*S_A**(3/2)*np.array([1, -1])),
         Interaction([site_iA3], -prefactor_1*S_A**(3/2)*np.array([1, -1])),
         Interaction([site_iA], prefactor_1*S_A**(3/2)*np.array([1, -1])),
         Interaction([site_iA1], -prefactor_1*S_A**(3/2)*np.array([1, -1])),
         Interaction([site_iA], prefactor_1*S_A**(3/2)*np.array([1, -1])),
         Interaction([site_iA4], -prefactor_1*S_A**(3/2)*np.array([1, -1])),
+
         Interaction([site_iB], -prefactor_1*S_B**(3/2)*np.array([1, -1])),
         Interaction([site_iB3], prefactor_1*S_B**(3/2)*np.array([1, -1])),
         Interaction([site_iB], -prefactor_1*S_B**(3/2)*np.array([1, -1])),
@@ -201,6 +204,7 @@ def test_real_space_Hamiltonian_honeycomb_DMI():
         Interaction([site_iB1]*2, np.array([[0, 0], [-S_A*J, 0]])),
         Interaction([site_iA]*2, np.array([[0, 0], [-S_B*J, 0]])),
         Interaction([site_iB2]*2, np.array([[0, 0], [-S_A*J, 0]])),
+
         # Heisenberg -- hopping terms
         Interaction([site_iA, site_iB], np.array([[0, np.sqrt(S_A*S_B)*J], [0, 0]])),
         Interaction([site_iB, site_iA], np.array([[0, np.sqrt(S_A*S_B)*J], [0, 0]])),
@@ -208,13 +212,15 @@ def test_real_space_Hamiltonian_honeycomb_DMI():
         Interaction([site_iB1, site_iA], np.array([[0, np.sqrt(S_A*S_B)*J], [0, 0]])),
         Interaction([site_iA, site_iB2], np.array([[0, np.sqrt(S_A*S_B)*J], [0, 0]])),
         Interaction([site_iB2, site_iA], np.array([[0, np.sqrt(S_A*S_B)*J], [0, 0]])),
-        # DMI -- hopping terms
+
+        # DMI-z -- hopping terms
         Interaction([site_iA, site_iA3], prefactor_2*S_A*np.array([[0, 1], [0, 0]])),
         Interaction([site_iA3, site_iA], -prefactor_2*S_A*np.array([[0, 1], [0, 0]])),
         Interaction([site_iA, site_iA1], prefactor_2*S_A*np.array([[0, 1], [0, 0]])),
         Interaction([site_iA1, site_iA], -prefactor_2*S_A*np.array([[0, 1], [0, 0]])),
         Interaction([site_iA, site_iA4], prefactor_2*S_A*np.array([[0, 1], [0, 0]])),
         Interaction([site_iA4, site_iA], -prefactor_2*S_A*np.array([[0, 1], [0, 0]])),
+
         Interaction([site_iB, site_iB3], -prefactor_2*S_B*np.array([[0, 1], [0, 0]])),
         Interaction([site_iB3, site_iB], prefactor_2*S_B*np.array([[0, 1], [0, 0]])),
         Interaction([site_iB, site_iB4], -prefactor_2*S_B*np.array([[0, 1], [0, 0]])),
@@ -223,6 +229,117 @@ def test_real_space_Hamiltonian_honeycomb_DMI():
         Interaction([site_iB2, site_iB], prefactor_2*S_B*np.array([[0, 1], [0, 0]])),
     ]
 
+    # order S^(1/2)
+    prefactor_3 = D*np.sin(theta)*1j/np.sqrt(2)
+    caa = np.array([[[0, 0], [0, 0]], [[1, 0], [0, 0]]])
+    aca = np.array([[[0, 0], [1, 0]], [[0, 0], [0, 0]]])
+    cca = np.array([[[0, 0], [0, 0]], [[0, 0], [1, 0]]])
+    cac = np.array([[[0, 0], [0, 0]], [[0, 1], [0, 0]]])
+    expected_magnon_H_3 = [
+        # DMI-x -- on-site terms
+        Interaction([site_iA, site_iA, site_iA], -1/4*prefactor_3*S_A**(1/2)*(caa - cca)),
+        Interaction([site_iA3, site_iA3, site_iA3], 1/4*prefactor_3*S_A**(1/2)*(caa - cca)),
+        Interaction([site_iA, site_iA, site_iA], -1/4*prefactor_3*S_A**(1/2)*(caa - cca)),
+        Interaction([site_iA1, site_iA1, site_iA1], 1/4*prefactor_3*S_A**(1/2)*(caa - cca)),
+        Interaction([site_iA, site_iA, site_iA], -1/4*prefactor_3*S_A**(1/2)*(caa - cca)),
+        Interaction([site_iA4, site_iA4, site_iA4], 1/4*prefactor_3*S_A**(1/2)*(caa - cca)),
+
+        Interaction([site_iB, site_iB, site_iB], 1/4*prefactor_3*S_B**(1/2)*(caa - cca)),
+        Interaction([site_iB3, site_iB3, site_iB3], -1/4*prefactor_3*S_B**(1/2)*(caa - cca)),
+        Interaction([site_iB, site_iB, site_iB], 1/4*prefactor_3*S_B**(1/2)*(caa - cca)),
+        Interaction([site_iB4, site_iB4, site_iB4], -1/4*prefactor_3*S_B**(1/2)*(caa - cca)),
+        Interaction([site_iB, site_iB, site_iB], 1/4*prefactor_3*S_B**(1/2)*(caa - cca)),
+        Interaction([site_iB2, site_iB2, site_iB2], -1/4*prefactor_3*S_B**(1/2)*(caa - cca)),
+
+        # DMI-x -- hopping terms
+        Interaction([site_iA, site_iA, site_iA3], prefactor_3*S_A**(1/2)*caa),
+        Interaction([site_iA3, site_iA, site_iA], -prefactor_3*S_A**(1/2)*cca),
+        Interaction([site_iA, site_iA3, site_iA3], -prefactor_3*S_A**(1/2)*aca),
+        Interaction([site_iA3, site_iA3, site_iA], prefactor_3*S_A**(1/2)*cac),
+        Interaction([site_iA, site_iA, site_iA1], prefactor_3*S_A**(1/2)*caa),
+        Interaction([site_iA1, site_iA, site_iA], -prefactor_3*S_A**(1/2)*cca),
+        Interaction([site_iA, site_iA1, site_iA1], -prefactor_3*S_A**(1/2)*aca),
+        Interaction([site_iA1, site_iA1, site_iA], prefactor_3*S_A**(1/2)*cac),
+        Interaction([site_iA, site_iA, site_iA4], prefactor_3*S_A**(1/2)*caa),
+        Interaction([site_iA4, site_iA, site_iA], -prefactor_3*S_A**(1/2)*cca),
+        Interaction([site_iA, site_iA4, site_iA4], -prefactor_3*S_A**(1/2)*aca),
+        Interaction([site_iA4, site_iA4, site_iA], prefactor_3*S_A**(1/2)*cac),
+
+        Interaction([site_iB, site_iB, site_iB3], -prefactor_3*S_B**(1/2)*caa),
+        Interaction([site_iB3, site_iB, site_iB], prefactor_3*S_B**(1/2)*cca),
+        Interaction([site_iB, site_iB3, site_iB3], prefactor_3*S_B**(1/2)*aca),
+        Interaction([site_iB3, site_iB3, site_iB], -prefactor_3*S_B**(1/2)*cac),
+        Interaction([site_iB, site_iB, site_iB4], -prefactor_3*S_B**(1/2)*caa),
+        Interaction([site_iB4, site_iB, site_iB], prefactor_3*S_B**(1/2)*cca),
+        Interaction([site_iB, site_iB4, site_iB4], prefactor_3*S_B**(1/2)*aca),
+        Interaction([site_iB4, site_iB4, site_iB], -prefactor_3*S_B**(1/2)*cac),
+        Interaction([site_iB, site_iB, site_iB2], -prefactor_3*S_B**(1/2)*caa),
+        Interaction([site_iB2, site_iB, site_iB], prefactor_3*S_B**(1/2)*cca),
+        Interaction([site_iB, site_iB2, site_iB2], prefactor_3*S_B**(1/2)*aca),
+        Interaction([site_iB2, site_iB2, site_iB], -prefactor_3*S_B**(1/2)*cac),
+    ]
+
+    # order S^0
+    prefactor_4 = D*np.cos(theta)*1j/4
+    caca = np.zeros((2, 2, 2, 2))
+    caca[1, 0, 1, 0] = 1.0
+    acca = np.zeros((2, 2, 2, 2))
+    acca[0, 1, 1, 0] = 1.0
+    caac = np.zeros((2, 2, 2, 2))
+    caac[1, 0, 0, 1] = 1.0
+    expected_magnon_H_4 = [
+        # Heisenberg -- on-site terms
+        Interaction([site_iA, site_iA, site_iB, site_iB], J*caca),
+        Interaction([site_iA, site_iA, site_iB1, site_iB1], J*caca),
+        Interaction([site_iA, site_iA, site_iB2, site_iB2], J*caca),
+
+        # Heisenberg -- hopping terms
+        Interaction([site_iA, site_iB, site_iB, site_iB], -J/4*(S_A/S_B)**(1/2)*acca),
+        Interaction([site_iB, site_iB, site_iB, site_iA], -J/4*(S_A/S_B)**(1/2)*caac),
+        Interaction([site_iA, site_iA, site_iA, site_iB], -J/4*(S_B/S_A)**(1/2)*caac),
+        Interaction([site_iB, site_iA, site_iA, site_iA], -J/4*(S_B/S_A)**(1/2)*acca),
+        Interaction([site_iA, site_iB1, site_iB1, site_iB1], -J/4*(S_A/S_B)**(1/2)*acca),
+        Interaction([site_iB1, site_iB1, site_iB1, site_iA], -J/4*(S_A/S_B)**(1/2)*caac),
+        Interaction([site_iA, site_iA, site_iA, site_iB1], -J/4*(S_B/S_A)**(1/2)*caac),
+        Interaction([site_iB1, site_iA, site_iA, site_iA], -J/4*(S_B/S_A)**(1/2)*acca),
+        Interaction([site_iA, site_iB2, site_iB2, site_iB2], -J/4*(S_A/S_B)**(1/2)*acca),
+        Interaction([site_iB2, site_iB2, site_iB2, site_iA], -J/4*(S_A/S_B)**(1/2)*caac),
+        Interaction([site_iA, site_iA, site_iA, site_iB2], -J/4*(S_B/S_A)**(1/2)*caac),
+        Interaction([site_iB2, site_iA, site_iA, site_iA], -J/4*(S_B/S_A)**(1/2)*acca),
+
+        # DMI-z -- hopping terms
+        Interaction([site_iA, site_iA3, site_iA3, site_iA3], -prefactor_4*acca),
+        Interaction([site_iA3, site_iA3, site_iA3, site_iA], prefactor_4*caac),
+        Interaction([site_iA, site_iA, site_iA, site_iA3], -prefactor_4*caac),
+        Interaction([site_iA3, site_iA, site_iA, site_iA], prefactor_4*acca),
+        Interaction([site_iA, site_iA1, site_iA1, site_iA1], -prefactor_4*acca),
+        Interaction([site_iA1, site_iA1, site_iA1, site_iA], prefactor_4*caac),
+        Interaction([site_iA, site_iA, site_iA, site_iA1], -prefactor_4*caac),
+        Interaction([site_iA1, site_iA, site_iA, site_iA], prefactor_4*acca),
+        Interaction([site_iA, site_iA4, site_iA4, site_iA4], -prefactor_4*acca),
+        Interaction([site_iA4, site_iA4, site_iA4, site_iA], prefactor_4*caac),
+        Interaction([site_iA, site_iA, site_iA, site_iA4], -prefactor_4*caac),
+        Interaction([site_iA4, site_iA, site_iA, site_iA], prefactor_4*acca),
+        
+        Interaction([site_iB, site_iB3, site_iB3, site_iB3], prefactor_4*acca),
+        Interaction([site_iB3, site_iB3, site_iB3, site_iB], -prefactor_4*caac),
+        Interaction([site_iB, site_iB, site_iB, site_iB3], prefactor_4*caac),
+        Interaction([site_iB3, site_iB, site_iB, site_iB], -prefactor_4*acca),
+        Interaction([site_iB, site_iB4, site_iB4, site_iB4], prefactor_4*acca),
+        Interaction([site_iB4, site_iB4, site_iB4, site_iB], -prefactor_4*caac),
+        Interaction([site_iB, site_iB, site_iB, site_iB4], prefactor_4*caac),
+        Interaction([site_iB4, site_iB, site_iB, site_iB], -prefactor_4*acca),
+        Interaction([site_iB, site_iB2, site_iB2, site_iB2], prefactor_4*acca),
+        Interaction([site_iB2, site_iB2, site_iB2, site_iB], -prefactor_4*caac),
+        Interaction([site_iB, site_iB, site_iB, site_iB2], prefactor_4*caac),
+        Interaction([site_iB2, site_iB, site_iB, site_iB], -prefactor_4*acca),
+    ]
+
+
     assert_all_real_space_Hamiltonians_equal(model, [
-        expected_magnon_H_0, expected_magnon_H_1, expected_magnon_H_2, #[], expected_magnon_H_4,
+        expected_magnon_H_0, 
+        expected_magnon_H_1,
+        expected_magnon_H_2, 
+        expected_magnon_H_3, 
+        expected_magnon_H_4,
     ])
