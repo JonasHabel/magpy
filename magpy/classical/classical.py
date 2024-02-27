55cad68c4506bdb9a03b3e8726580167d35fa595
Jonas Habel <jonas.habel@tum.de>
2024-02-27 18:52:13 +0100

    Speedup of classical spin gradient computation using jit

--- magpy/classical/classical.py
+++ magpy/classical/classical.py
@@ -3,6 +3,8 @@ from magpy.lattice import BravaisLattice
 from magpy.models import Model
 from magpy.interactions import Interaction
 from magpy import util
+from magpy.classical.util import convert_to_flat_index
+from numba import njit
 
 
 def compute_total_energy(model: Model, spin_config):
@@ -55,36 +57,56 @@ def compute_total_energy(model: Model, spin_config):
 def compute_spin_gradient(lattice: BravaisLattice, spin_config):
     assert spin_config.shape[-1] == 3
 
-    lattice_dims = spin_config.shape[:-2]
-    bravais_coords = \
-        lattice.sample_Bravais_lattice_in_Bravais_coords(lattice_dims)
+    @njit
+    def compute_spin_gradient_for_edge(
+            spin_gradient_out_flat, spin_config_flat, 
+            edge_bravais_coords, edge_vector, edge_subl_idxs, 
+            bravais_coords_lattice, lattice_sizes, num_sublattices):
+        edge_length_sq = np.sum(edge_vector**2)
+        for bravais_coord in bravais_coords_lattice:
+            spin1_bravais_coord_pbc = \
+                bravais_coord % lattice_sizes
+            spin2_bravais_coord_pbc = \
+                (bravais_coord + edge_bravais_coords) % lattice_sizes
+            
+            spin1_flat_idx = convert_to_flat_index(
+                spin1_bravais_coord_pbc, edge_subl_idxs[0], 
+                lattice_sizes, num_sublattices)
+            spin2_flat_idx = convert_to_flat_index(
+                spin2_bravais_coord_pbc, edge_subl_idxs[1], 
+                lattice_sizes, num_sublattices)
+            spin1 = spin_config_flat[spin1_flat_idx]
+            spin2 = spin_config_flat[spin2_flat_idx]
+            spin_derivative = (spin2 - spin1) / edge_length_sq
+
+            spin_gradient_out_flat[:, spin1_flat_idx] += \
+                np.outer(edge_vector, spin_derivative)
+            spin_gradient_out_flat[:, spin2_flat_idx] += \
+                np.outer(edge_vector, spin_derivative)
+            
+            
+    lattice_sizes = spin_config.shape[:-2]
+    num_unit_cells = int(np.prod(lattice_sizes))
+    num_sublattices = lattice.num_sites_unit_cell
+    num_sites_total = num_unit_cells * num_sublattices
     
-    spin_gradient = np.zeros((2, *spin_config.shape))
+    spin_config_flat = spin_config.reshape((num_sites_total, 3))
+    bravais_coords_lattice = \
+        lattice.sample_Bravais_lattice_in_Bravais_coords(lattice_sizes)
+    
+    spin_gradient_flat = np.zeros((2, *spin_config_flat.shape))
 
     for edge in lattice.edges:
         edge_vector = lattice.get_canonical_coords_for_edge(edge)
-        edge_length_sq = np.sum(edge_vector**2)
-
-        for bravais_coord in bravais_coords:
-            bravais_coord_pbc = \
-                bravais_coord % np.array(lattice_dims)
-            other_bravais_coord_pbc = \
-                (bravais_coord + edge.bravais_coords) % np.array(lattice_dims)
-            spin1 = spin_config[(
-                *bravais_coord_pbc, 
-                edge.subl_idxs[0]
-            )]
-            spin2 = spin_config[(
-                *other_bravais_coord_pbc, 
-                edge.subl_idxs[1]
-            )]
-            spin_derivative = (spin2 - spin1) / edge_length_sq
-            spin_gradient[(slice(None), *bravais_coord_pbc, edge.subl_idxs[0])] += \
-                np.outer(edge_vector, spin_derivative)
-            spin_gradient[(slice(None), *other_bravais_coord_pbc, edge.subl_idxs[1])] += \
-                np.outer(edge_vector, spin_derivative)
+        compute_spin_gradient_for_edge(
+            spin_gradient_flat, spin_config_flat, 
+            edge.bravais_coords, edge_vector, edge.subl_idxs, 
+            bravais_coords_lattice, np.array(lattice_sizes), num_sublattices)
+        
 
     # TODO normalization
+        
+    spin_gradient = spin_gradient_flat.reshape((2, *spin_config.shape))
 
     return spin_gradient
 
