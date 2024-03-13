35d1c72931047ead0c5b91762366b144bb4165f9
Jonas Habel <ga63hef@ph.tum.de>
2024-03-13 14:07:16 +0100

    Added momentum range option to density of states plot

--- magpy/plot/density_of_states_plot.py
+++ magpy/plot/density_of_states_plot.py
@@ -6,19 +6,21 @@ from .plot_util import set_momentum_path_x_ticks
 
 def plot_n_magnon_density_of_states_along_momentum_path(
         momentum_path: ReciprocalLattice.MomentumPath,
-        frequencies, n_magnon_dos, params={}):
+        frequencies, n_magnon_dos, krange=None, params={}):
     fig, ax = plt.subplots()
     plot_ax_n_magnon_density_of_states_along_momentum_path(
-        momentum_path, frequencies, n_magnon_dos, params, fig, ax)
+        momentum_path, frequencies, n_magnon_dos, krange, params, fig, ax)
     set_momentum_path_x_ticks(ax, momentum_path)
     plt.show()
 
 
 def plot_ax_n_magnon_density_of_states_along_momentum_path(
         momentum_path: ReciprocalLattice.MomentumPath,
-        frequencies, n_magnon_dos, params={}, fig=plt.gcf(), ax=plt.gca()):
-    c = ax.contourf(np.arange(len(momentum_path.ks)),
-                frequencies, n_magnon_dos.T, **params)
+        frequencies, n_magnon_dos, krange=None, params={}, fig=plt.gcf(), ax=plt.gca()):
+    if krange is None:
+        krange = (0, len(momentum_path.ks))
+    c = ax.contourf(np.arange(*krange),
+                    frequencies, n_magnon_dos.T, **params)
     cbar = fig.colorbar(c)
     
     return fig, ax, cbar
