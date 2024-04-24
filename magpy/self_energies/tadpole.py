77063e68f55258656e67aa7b7c1caf85f8f5f26b
Jonas Habel <jonas.habel@tum.de>
2024-04-24 19:15:54 +0200

    Fixed gauge phases for tadpole diagram

--- magpy/self_energies/tadpole.py
+++ magpy/self_energies/tadpole.py
@@ -4,19 +4,24 @@ from numba import njit
 from ..models import *
 from ..greens_functions import get_free_propagator_zero_T
 from .util import *
+from ..util import PAULI_MATRICES
 
 
 
 def compute_one_magnon_self_energy(
         frequencies,
         energies_Gamma,
+        eigvs_k,
         cubic_verts,
+        eigvs_verts,
         linear_commutator_terms,
+        eigvs_commutator_terms,
         num_ks_BZ,
         T, 
         ph_labels,
         reg):
     
+    # SETUP
     # 1 for particle, 0 for hole
     ph_idxs = convert_ph_labels_to_indices(ph_labels)
     ph_idxs_vert = \
@@ -30,12 +35,38 @@ def compute_one_magnon_self_energy(
     linear_comm_term = linear_commutator_terms[to_binary(ph_idxs_comm_term)]
     pos_energies_Gamma = energies_Gamma[::2]
 
+    # COMPUTE BDG GAUGE PHASES FOR THE CONTRACTED LEGS
+    HOLE, PARTICLE = 0, 1
+    sigma_x = PAULI_MATRICES[0]
+    def conjugate_if(eigvs, condition):
+        return sigma_x @ eigvs.conj() @ sigma_x if condition() else eigvs
+    
+    eigvs_in = conjugate_if(eigvs_k, lambda: ph_idxs[0][0] == PARTICLE)
+    eigvs_out = conjugate_if(eigvs_k, lambda: ph_idxs[2][0] == HOLE)
+    gauge_phase_in = compute_gauge_phase(eigvs_in, eigvs_verts[0])[::ph_idxs[0][0]]
+    gauge_phase_internal = compute_gauge_phase(eigvs_verts[1], eigvs_commutator_terms[0])[::ph_idxs[1][0]]
+    gauge_phase_out = compute_gauge_phase(eigvs_verts[2], eigvs_out)[::ph_idxs[2][0]]
+
+    # EVALUATE DIAGRAM
     self_energy = np.zeros((num_freqs, num_bands, num_bands),
                             dtype=np.complex128)
     
     self_energy[:] = np.einsum(
-        "mln,l,l->nm", 
-        cubic_vert, 1.0/(-pos_energies_Gamma + 1j*reg), linear_comm_term)[np.newaxis]
+        "n,mln,l,l,l,m->nm", 
+        gauge_phase_in,
+        cubic_vert, 
+        gauge_phase_internal,
+        1.0/(-pos_energies_Gamma + 1j*reg),
+        linear_comm_term,
+        gauge_phase_out)[np.newaxis]
     
     return self_energy / np.prod(num_ks_BZ)
-    
+
+
+
+
+def compute_gauge_phase(eigvs_1, eigvs_2):
+    sigma_x, sigma_z = PAULI_MATRICES[0], PAULI_MATRICES[2]
+    eigvs_1_inv = sigma_z @ eigvs_1.T.conj() @ sigma_z
+
+    return np.diag(eigvs_1_inv @ sigma_x @ eigvs_2.conj() @ sigma_x)
