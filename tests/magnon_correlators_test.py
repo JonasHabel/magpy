afd31c184d7c2f1015463bbc10a7531ea0d5ce51
Jonas Habel <jonas.habel@tum.de>
2024-08-20 22:59:27 +0200

    Started implementing tests for magnon correlators

---
+++ tests/magnon_correlators_test.py
@@ -0,0 +1,99 @@
+import numpy as np
+from magpy.models import Model
+from magpy.largeS import LSWT
+from magpy.momenta_utils import Momenta
+from magpy.correlators import magnon_correlators
+from . import test_models
+
+
+def test_correlators_FM_Heisenberg_chain():
+    model, _ = test_models.FM_Heisenberg_chain()
+
+    N_BZ = 10,
+    ks_BZ = Momenta.of_BZ(model.lattice, N_BZ)
+    _, eigvs_BZ = LSWT.get_eigensystems_momentum_space(model, ks_BZ)
+    
+    # MOMENTUM SPACE
+    correlators_mom_space = magnon_correlators.compute_momentum_space_correlators_LSWT(eigvs_BZ, strip=True).raw_quantity
+
+    assert correlators_mom_space.shape == (*N_BZ, 2, 2)
+    assert np.allclose(
+        correlators_mom_space, 
+        np.tile(np.array([[0, 1], [0, 0]]), (*N_BZ, 1, 1)),
+    )
+
+    # REAL SPACE
+    N_sites = N_BZ
+    bravais_coords = model.lattice.sample_Bravais_lattice_in_Bravais_coords(N_sites)
+    correlators_real_space = \
+        magnon_correlators.compute_real_space_correlator_LSWT(
+            ks_BZ, eigvs_BZ, bravais_coords
+        )
+    
+    expected_correlators_real_space = np.zeros((*N_sites, 2, 2))
+    expected_correlators_real_space[0, 0, 1] = 1.
+    assert correlators_real_space.shape == (*N_sites, 2, 2)
+    assert np.allclose(
+        correlators_real_space,
+        expected_correlators_real_space,
+    )
+
+
+
+
+def test_correlators_AFM_Heisenberg_chain():
+    model, params = test_models.AFM_Heisenberg_chain()
+    J, S_A, S_B = params
+
+    N_BZ = 10,
+    ks_BZ = Momenta.of_BZ(model.lattice, N_BZ)
+    _, eigvs_BZ = LSWT.get_eigensystems_momentum_space(model, ks_BZ)
+    
+    # MOMENTUM SPACE
+    correlators_mom_space = magnon_correlators.compute_momentum_space_correlators_LSWT(eigvs_BZ, strip=True).raw_quantity
+
+    # BdG Hamiltonian of the form [[epsilon, gamma*], [gamma, epsilon]]
+    epsilon = J*(S_A + S_B)
+    gamma = lambda k: J*np.sqrt(S_A*S_B) * (1 + np.exp(1j*k[0]))
+    # BdG transformation of the form U_k = [[u_k, v_k], [v_k*, u_k*]]
+    # where u_k = cosh(beta) and v_k = e^(i*phi)*sinh(beta) (up to global phase)
+    beta = lambda k: 0.5 * np.arctanh(-np.abs(gamma(k)) / epsilon)
+    phi = lambda k: -np.angle(gamma(k))
+    u, v = BdG_coeffs_u, BdG_coeffs_v = np.array([
+        [np.cosh(beta(k)), np.exp(1j*phi(k)) * np.sinh(beta(k))]
+        for k in ks_BZ.k_arrays[0]
+    ]).T    # u, v is a short-hand for the BdG coefficients
+
+    assert correlators_mom_space.shape == (*N_BZ, 4, 4)
+    # assert np.allclose(
+    #     correlators_mom_space, 
+    #     np.array([
+    #         [
+    #             [0, ..., ..., 0],   # <aa>   <aa^†>,  <ab>   <ab^†>
+    #             [..., 0, 0, ...],   # <a^†a> <a^†a^†> <a^†b> <a^†b^†>
+    #             [..., 0, 0, ...],   # <ba>   <ba^†>   <bb>   <bb^†>
+    #             [0, ..., ..., 0],   # <b^†a> <b^†a^†> <b^†b> <b^†b^†>
+    #         ] for nk, k in enumerate(ks_BZ.k_arrays[0])
+    #     ]),
+    # )
+    
+    # REAL SPACE
+    N_sites = N_BZ
+    bravais_coords = model.lattice.sample_Bravais_lattice_in_Bravais_coords(N_sites)
+    correlators_real_space = \
+        magnon_correlators.compute_real_space_correlator_LSWT(
+            ks_BZ, eigvs_BZ, bravais_coords
+        )
+    
+    expected_correlators_real_space = np.zeros((*N_sites, 4, 4))
+    # expected_correlators_real_space[0] = np.array([
+    #     [0, ..., ..., 0],
+    #     [..., 0, 0, ...],
+    #     [..., 0, 0, ...],
+    #     [0, ..., ..., 0],
+    # ])
+    assert correlators_real_space.shape == (*N_sites, 4, 4)
+    # assert np.allclose(
+    #     correlators_real_space,
+    #     expected_correlators_real_space
+    # )
