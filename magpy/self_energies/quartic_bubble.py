da8dffd2f11c33d1c6a455894b42d9b671928381
Jonas Habel <jonas.habel@tum.de>
2024-08-23 20:14:03 +0200

    Removed debug print statements from quartic bubble self-energy

--- magpy/self_energies/quartic_bubble.py
+++ magpy/self_energies/quartic_bubble.py
@@ -22,20 +22,19 @@ def compute_one_magnon_self_energy(
     ph_idxs = convert_ph_labels_to_indices(ph_labels)
     ph_idxs_comm_term = \
         np.array([ph_idxs[1][0], 1-ph_idxs[0][0]])
-    print("PH ID\n", ph_idxs, ph_idxs_comm_term, to_binary(ph_idxs_comm_term), "\n\n")
+    
     num_freqs = len(frequencies)
     num_bands = quadratic_commutator_terms.shape[-1]
     quadratic_comm_term = quadratic_commutator_terms[to_binary(ph_idxs_comm_term)]
-    print("COMM_TERM\n", quadratic_comm_term, "\n\n")
+    
     # COMPUTE BDG GAUGE PHASES FOR THE CONTRACTED LEGS
     HOLE, PARTICLE = 0, 1
-    print("EIGVS_K\n", eigvs_k, "\n\n") 
+    
     eigvs_in = conjugate_if(eigvs_k, lambda: ph_idxs[0][0] == PARTICLE)
     eigvs_out = conjugate_if(eigvs_k, lambda: ph_idxs[1][0] == HOLE)
     gauge_phase_in = compute_gauge_phase(eigvs_commutator_terms[1], eigvs_in)[1-ph_idxs[0][0]::2, 1-ph_idxs[0][0]::2]
     gauge_phase_out = compute_gauge_phase(eigvs_commutator_terms[0], eigvs_out)[ph_idxs[1][0]::2, ph_idxs[1][0]::2]
-    print("G_IN\n", gauge_phase_in, "\n\n")
-    print("G_OUT\n", gauge_phase_out, "\n\n")
+    
     # EVALUATE DIAGRAM
     self_energy = np.zeros((num_freqs, num_bands, num_bands),
                             dtype=np.complex128)
@@ -52,12 +51,7 @@ def compute_one_magnon_self_energy(
         ph_idxs_loops=[],
         num_internal_propagators=0,
     )
-    print(ph_idxs_comm_term)
-    print("PREFACTOR\n", compute_diagram_prefactor(
-        ph_idxs_verts=[ph_idxs_comm_term],
-        ph_idxs_loops=[],
-        num_internal_propagators=0,
-    )) 
+    
     return self_energy / np.prod(num_ks_BZ)
 
 
