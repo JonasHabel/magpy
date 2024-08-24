7b5244ed4f3740d2b252900383151aa34f3671a3
Jonas Habel <jonas.habel@tum.de>
2024-08-24 12:37:15 +0200

    Adapted pole equation solver tests

--- tests/greens_functions_test.py
+++ tests/greens_functions_test.py
@@ -38,12 +38,12 @@ def test_two_band_interacting_greens_functions_multiple_frequencies():
 
 
 def test_pole_equation_gradient_descent_non_interacting():
-    LSWT_energies = np.array([1., 3.,])
-    num_energies = len(LSWT_energies)
+    LSWT_energies = np.array([1., -1., 3., -3.])
+    num_bands = len(LSWT_energies) // 2
     reg = 0.01
     close_enough = 1e-3
     def se_and_deriv(omega):
-        return np.zeros((num_energies,)*2), np.zeros((num_energies,)*2)
+        return np.zeros((num_bands,)*2), np.zeros((num_bands,)*2)
 
     for init_freq in (0.4, 1.5, 2.3, 4.5, 20.0):
         solution = pole_equation.solve_by_gradient_descent(
@@ -60,15 +60,16 @@ def test_pole_equation_gradient_descent_non_interacting():
 
 
 def test_pole_equation_gradient_descent_band_hybridization():
-    LSWT_energies = np.array([1., 3.,])
-    num_energies = len(LSWT_energies)
+    LSWT_energies = np.array([1., -1., 3., -3.])
+    pos_LSWT_energies = LSWT_energies[::2]
+    num_bands = len(LSWT_energies) // 2
     V = 0.1
-    E_renormalized = np.sum(LSWT_energies)/2 + np.array([1, -1]) * np.sqrt((LSWT_energies[1] - LSWT_energies[0])**2 / 4 + V*np.conj(V))
+    E_renormalized = np.sum(pos_LSWT_energies)/2 + np.array([1, -1]) * np.sqrt((pos_LSWT_energies[1] - pos_LSWT_energies[0])**2 / 4 + V*np.conj(V))
     reg = 0.01
     close_enough = 1e-3
     def se_and_deriv(omega):
         return np.array([[0, np.conj(V)], [V, 0]]), \
-               np.zeros((num_energies,)*2)
+               np.zeros((num_bands,)*2)
 
     for init_freq in (0.0, 0.4, 1.5, 2.3, 4.5, 20.0):
         solution = pole_equation.solve_by_gradient_descent(
@@ -83,8 +84,8 @@ def test_pole_equation_gradient_descent_band_hybridization():
 
 
     def se_and_deriv(omega):
-        return np.diag(V*np.conj(V) / (omega - LSWT_energies[::-1] + 1j*reg)), \
-               np.diag(V*np.conj(V) / (omega - LSWT_energies[::-1] + 1j*reg)**2),
+        return np.diag(V*np.conj(V) / (omega - pos_LSWT_energies[::-1] + 1j*reg)), \
+               np.diag(V*np.conj(V) / (omega - pos_LSWT_energies[::-1] + 1j*reg)**2),
     
     for init_freq in (0.0, 0.4, 1.5, 2.3, 4.5, 20.0):
         solution = pole_equation.solve_by_gradient_descent(
@@ -111,7 +112,7 @@ def test_pole_equation_gradient_descent_BdG_interaction():
 
     for init_freq in (-1.0, 0.0, 1.0, 2.1):
         solution = pole_equation.solve_by_gradient_descent(
-            init_freq=init_freq, LSWT_energies=np.array([E]),
+            init_freq=init_freq, LSWT_energies=np.array([E, -E]),
             compute_self_energies_and_derivative_at_freq=se_and_deriv,
             reg=reg, num_steps=100, step_size=0.2, eps=1e-5,
         )
