398f19089530f29338dcdfb6f8647c15a2bcefb3
Jonas Habel <jonas.habel@tum.de>
2025-02-04 17:34:37 +0100

    Added functions to compute full tadpole/quartic bubble self-energies, including anomalous contributions. Removed regulator from tadpole internal propagator (fuck the Goldstone mode)

--- magpy/self_energies/quartic_bubble.py
+++ magpy/self_energies/quartic_bubble.py
@@ -7,6 +7,64 @@ from .util import *
 from ..util import PAULI_MATRICES
 
 
+def compute_full_one_magnon_self_energy(
+    frequencies,
+    eigvs_k,
+    quadratic_commutator_terms,
+    eigvs_commutator_terms,
+    num_ks_BZ,
+    T,
+):
+    # SETUP
+    num_freqs = len(frequencies)
+    num_bands = quadratic_commutator_terms.shape[-1]
+
+    ANNIHILATOR, CREATOR = 0, 1
+    quadratic_comm_term = np.zeros(
+        (*quadratic_commutator_terms.shape[1:-2], 2*num_bands, 2*num_bands), 
+        dtype=np.complex128,
+    )
+    quadratic_comm_term[..., 0::2, 0::2] = \
+        quadratic_commutator_terms[to_binary([CREATOR, ANNIHILATOR])]
+    quadratic_comm_term[..., 1::2, 0::2] = \
+        quadratic_commutator_terms[to_binary([ANNIHILATOR, ANNIHILATOR])]
+    quadratic_comm_term[..., 0::2, 1::2] = \
+        quadratic_commutator_terms[to_binary([CREATOR, CREATOR])]
+    quadratic_comm_term[..., 1::2, 1::2] = \
+        quadratic_commutator_terms[to_binary([ANNIHILATOR, CREATOR])]
+    
+    # COMPUTE BDG GAUGE PHASES FOR THE CONTRACTED LEGS
+    eigvs_in = BdG_conjugate(eigvs_k)
+    eigvs_out = eigvs_k
+    sigma_x = np.kron(np.eye(num_bands), PAULI_MATRICES[0])
+    gauge_phase_in = compute_gauge_phase(eigvs_commutator_terms[1], eigvs_in)
+    gauge_phase_out = sigma_x @ compute_gauge_phase(eigvs_commutator_terms[0], eigvs_out) @ sigma_x
+    
+    # EVALUATE DIAGRAM
+    self_energy = np.zeros((num_freqs, 2*num_bands, 2*num_bands),
+                            dtype=np.complex128)
+    
+    self_energy[:] = np.einsum(
+        "Nn,MN,Mm->nm", 
+        gauge_phase_in,
+        quadratic_comm_term, 
+        gauge_phase_out)[np.newaxis]
+    
+    
+    # apply diagram prefactor for each particle/hole sector separately
+    for ph_idx_in in (ANNIHILATOR, CREATOR):
+        for ph_idx_out in (ANNIHILATOR, CREATOR):
+            ph_idxs_comm_term = np.array([ph_idx_out, 1-ph_idx_in])
+            diagram_prefactor = compute_diagram_prefactor(
+                    ph_idxs_verts=[ph_idxs_comm_term],
+                    ph_idxs_loops=[],
+                    num_internal_propagators=0)
+            
+            self_energy[..., ph_idx_in::2, ph_idx_out::2] *= diagram_prefactor
+    
+    return self_energy / np.prod(num_ks_BZ)
+
+
 
 def compute_one_magnon_self_energy(
     frequencies,
--- magpy/self_energies/tadpole.py
+++ magpy/self_energies/tadpole.py
@@ -7,6 +7,88 @@ from .util import *
 from ..util import PAULI_MATRICES
 
 
+def compute_full_one_magnon_self_energy(
+    frequencies,
+    energies_Gamma,
+    eigvs_k,
+    cubic_verts,
+    eigvs_verts,
+    linear_commutator_terms,
+    eigvs_commutator_terms,
+    num_ks_BZ,
+    T, 
+    ph_labels_intermediate_state,
+    reg,
+):
+    # 1 for particle, 0 for hole
+    ph_idxs_intermediate_state = np.array(convert_ph_labels_to_indices(
+        [ph_labels_intermediate_state]
+    )[0])
+    ph_idxs_comm_term = 1 - ph_idxs_intermediate_state
+
+    num_freqs = len(frequencies)
+    num_bands = linear_commutator_terms.shape[-1]
+
+    ANNIHILATOR, CREATOR = 0, 1
+    cubic_vert = np.zeros(
+        (*cubic_verts.shape[1:-3], 2*num_bands, num_bands, 2*num_bands), 
+        dtype=np.complex128,
+    )
+    cubic_vert[..., 0::2, :, 0::2] = \
+        cubic_verts[to_binary([CREATOR, ph_idxs_intermediate_state[0], ANNIHILATOR])]   # -k-q, q, k
+    cubic_vert[..., 1::2, :, 0::2] = \
+        cubic_verts[to_binary([ANNIHILATOR, ph_idxs_intermediate_state[0], ANNIHILATOR])]
+    cubic_vert[..., 0::2, :, 1::2] = \
+        cubic_verts[to_binary([CREATOR, ph_idxs_intermediate_state[0], CREATOR])]
+    cubic_vert[..., 1::2, :, 1::2] = \
+        cubic_verts[to_binary([ANNIHILATOR, ph_idxs_intermediate_state[0], CREATOR])]
+    linear_comm_term = linear_commutator_terms[to_binary(ph_idxs_comm_term)]
+    pos_energies_Gamma = energies_Gamma[::2]
+
+    # COMPUTE BDG GAUGE PHASES FOR THE CONTRACTED LEGS
+    
+    eigvs_in = BdG_conjugate(eigvs_k)
+    eigvs_out = eigvs_k
+    sigma_x = np.kron(np.eye(num_bands), PAULI_MATRICES[0])
+    gauge_phase_in = compute_gauge_phase(eigvs_verts[2], eigvs_in)
+    gauge_phase_internal = compute_gauge_phase(eigvs_verts[1], eigvs_commutator_terms)[ph_idxs_intermediate_state[0]::2, ph_idxs_intermediate_state[0]::2]
+    gauge_phase_out = sigma_x @ compute_gauge_phase(eigvs_verts[0], eigvs_out) @ sigma_x
+
+    # EVALUATE DIAGRAM
+    self_energy = np.zeros((num_freqs, 2*num_bands, 2*num_bands),
+                            dtype=np.complex128)
+    
+    self_energy[:] = np.einsum(
+        "Nn,MLN,Ll,l,l,Mm->nm", 
+        gauge_phase_in,
+        cubic_vert, 
+        gauge_phase_internal,
+        1.0/(-pos_energies_Gamma),
+        linear_comm_term,
+        gauge_phase_out)[np.newaxis]
+    
+    # apply diagram prefactor for each particle/hole sector separately
+    HOLE, PARTICLE = 0, 1
+    for ph_idx_in in (HOLE, PARTICLE):
+        for ph_idx_out in (HOLE, PARTICLE):
+            ph_idxs_vert = \
+                np.array([ph_idx_out, ph_idxs_intermediate_state[0], 1-ph_idx_in])
+            diagram_prefactor = compute_diagram_prefactor(
+                    ph_idxs_verts=[ph_idxs_vert, ph_idxs_comm_term],
+                    ph_idxs_loops=[],
+                    num_internal_propagators=1)
+            
+            self_energy[..., 1-ph_idx_in::2, 1-ph_idx_out::2] *= diagram_prefactor
+
+    # self_energy *= compute_diagram_prefactor(
+    #     ph_idxs_verts=[ph_idxs_vert, ph_idxs_comm_term],
+    #     ph_idxs_loops=[],
+    #     num_internal_propagators=1,
+    # )
+    
+    return self_energy / np.prod(num_ks_BZ)
+
+
 
 def compute_one_magnon_self_energy(
     frequencies,
@@ -54,7 +136,7 @@ def compute_one_magnon_self_energy(
         gauge_phase_in,
         cubic_vert, 
         gauge_phase_internal,
-        1.0/(-pos_energies_Gamma + 1j*reg),
+        1.0/(-pos_energies_Gamma),
         linear_comm_term,
         gauge_phase_out)[np.newaxis]
     
