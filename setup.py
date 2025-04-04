484aa881fd29cea109795aca381fa456ccf1a003
Jonas Habel <jonas.habel@tum.de>
2025-04-04 18:16:58 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='magpy',
       author='Jonas Habel',
       author_email='',
       license='',
-      packages=['magpy', 'magpy.classical', 'magpy.file_io', 'magpy.interaction_vertices', 'magpy.largeS', 'magpy.plot', 'magpy.self_energies'],
+      packages=['magpy', 'magpy.classical', 'magpy.file_io', 'magpy.interaction_vertices', 'magpy.largeS', 'magpy.plot', 'magpy.self_energies', 'magpy.correlators'],
       zip_safe=False)
