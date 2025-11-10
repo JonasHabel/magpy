dd6568c087841fdf31a733309d28820e6a1fba09
Jonas Habel <ga63hef@ph.tum.de>
2025-11-10 20:18:10 +0100

    Bugfix in structure factor: now rotates the spins back from the local frame to the lab frame

--- magpy/structure_factors.py
+++ magpy/structure_factors.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.linalg import block_diag
 from magpy.models import Model
 
 
@@ -22,29 +23,37 @@ def compute_structure_factor(magnon_greens_functions, momentum, eigvs, model: Mo
 
     eigvs_conj = eigvs.T.conj()
     Hadamard = np.kron(np.eye(num_bands), np.array([[1, 1], [-1j, 1j]]))
+    gs_rot_mat = model.compute_ground_state_rotation_matrices()[:, :, 0:2]
+    gs_rot_mat_block = block_diag(*gs_rot_mat)
+
     # we need to transpose here because we accidentally chose a suboptimal
     # convention for the indices of the Green's function: G[..., in, out]
     # where the "standard" convention is G[..., out, in]
     GF_eigenspace_basis = magnon_greens_functions.transpose((0, 2, 1))
     GF_sublattice_basis = \
         np.einsum("sn,...nm,mt->...st", eigvs, GF_eigenspace_basis, eigvs_conj)
-    GF_spin_basis = \
+    GF_spin_basis_rotated_frame = \
         np.einsum("as,...st,tb->...ab", Hadamard, GF_sublattice_basis, Hadamard.T.conj())
+    GF_spin_basis_lab_frame = \
+        np.einsum("Aa,...ab,bB->...AB", gs_rot_mat_block, GF_spin_basis_rotated_frame, gs_rot_mat_block.T)
 
     S = model.get_onsite_spin_quantum_numbers()
     sublattice_offsets = model.lattice.sublattices
     u = np.sqrt(S) * np.exp(1j*sublattice_offsets.dot(momentum)) # the sublattice structure factor
-    u_matrix = np.kron(np.outer(u.conj(), u), np.eye(2))
+    u_matrix = np.kron(np.outer(u.conj(), u), np.eye(3))
 
-    # S_{αβ}(ω)   (α,β = x,y)
-    structure_factor = np.zeros((num_freqs, 2, 2), dtype=np.complex128)
+    # S_{αβ}(ω)   (α,β = x,y,z)
+    structure_factor = np.zeros((num_freqs, 3, 3), dtype=np.complex128)
 
-    for n, GF_for_freq in enumerate(GF_spin_basis):
-        # TODO prefactor??
+    for n, GF_for_freq in enumerate(GF_spin_basis_lab_frame):
+        # the "missing" minus sign in front of 0.5j is correct because our Greens function is
+        # actually defined as -1/(w - E - Sigma)
         GF_antiherm = 0.5j * (GF_for_freq - GF_for_freq.T.conj())
+
         # reshaped axes: (sublattice_i, spin-dir_i, sublattice_j, spin-dir_j)
+        # where spin-dir_i and spin-dir_j are in the lab frame (x, y, z)
         struct_fact_sublattice = \
-            (u_matrix * GF_antiherm).reshape((num_bands, 2, num_bands, 2))
+            (u_matrix * GF_antiherm).reshape((num_bands, 3, num_bands, 3))
         structure_factor[n] = np.sum(
             struct_fact_sublattice,
             axis=(0, 2),
@@ -53,8 +62,9 @@ def compute_structure_factor(magnon_greens_functions, momentum, eigvs, model: Mo
     return structure_factor
 
 
-
-def apply_kinetic_projector(structure_factor, momentum):
+def apply_kinetic_projector(structure_factor, momentum, model):
+    gs_rot_mat = model.compute_ground_state_rotation_matrices()[0]  
+    structure_factor_lab_frame = np.einsum("...AB,Aa,Bb->ab", structure_factor, )
     kinetic_projector = np.eye(2) - np.outer(momentum[0:2], momentum[0:2]) / momentum.dot(momentum)
 
     return np.einsum("...ab,ab", structure_factor, kinetic_projector)
